f7fe35ab57904de9b4cb70e189fb98dc4bdc892e
AdityaRathor <adityarathor120@gmail.com>
2024-01-03 22:04:18 +0530

    added login and register logic

--- netflixprj/netflixprj/settings.py
+++ netflixprj/netflixprj/settings.py
@@ -20,10 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-cj=hs_5jx9&63w4g0ypud@ws!^zpu*@gegy9p70pn)&8f(6p#&"
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
